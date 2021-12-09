6c7e350d2078017c4c65ada23d1ec94046848d9d
Amo97 <kacper.jedrzejewski@int.pl>
2021-12-09 08:26:51 +0100

    add web project with first own view

---
+++ CardTactics.Web/ClientApp/src/components/Table/Cards.js
@@ -0,0 +1,271 @@
+﻿import card10C from '../../img/cards/10C.png'
+import card10D from '../../img/cards/10C.png'
+import card10H from '../../img/cards/10C.png'
+import card10S from '../../img/cards/10C.png'
+import card2C from '../../img/cards/2C.png'
+import card2D from '../../img/cards/2D.png'
+import card2H from '../../img/cards/2H.png'
+import card2S from '../../img/cards/2S.png'
+import card2C from '../../img/cards/2C.png'
+import card3D from '../../img/cards/3D.png'
+import card3H from '../../img/cards/3H.png'
+import card3S from '../../img/cards/3S.png'
+import card3C from '../../img/cards/3C.png'
+import card4D from '../../img/cards/4D.png'
+import card4H from '../../img/cards/4H.png'
+import card4S from '../../img/cards/4S.png'
+import card4C from '../../img/cards/4C.png'
+import card5D from '../../img/cards/5D.png'
+import card5H from '../../img/cards/5H.png'
+import card5S from '../../img/cards/5S.png'
+import card5C from '../../img/cards/5C.png'
+import card6D from '../../img/cards/6D.png'
+import card6H from '../../img/cards/6H.png'
+import card6S from '../../img/cards/6S.png'
+import card6C from '../../img/cards/6C.png'
+import card7D from '../../img/cards/7D.png'
+import card7H from '../../img/cards/7H.png'
+import card7S from '../../img/cards/7S.png'
+import card7C from '../../img/cards/7C.png'
+import card8D from '../../img/cards/8D.png'
+import card8H from '../../img/cards/8H.png'
+import card8S from '../../img/cards/8S.png'
+import card8C from '../../img/cards/8C.png'
+import card9D from '../../img/cards/9D.png'
+import card9H from '../../img/cards/9H.png'
+import card9S from '../../img/cards/9S.png'
+import card9C from '../../img/cards/9C.png'
+import cardJD from '../../img/cards/JD.png'
+import cardJH from '../../img/cards/JH.png'
+import cardJS from '../../img/cards/JS.png'
+import cardJC from '../../img/cards/JC.png'
+import cardQD from '../../img/cards/QD.png'
+import cardQH from '../../img/cards/QH.png'
+import cardQS from '../../img/cards/QS.png'
+import cardQC from '../../img/cards/QC.png'
+import cardKD from '../../img/cards/KD.png'
+import cardKH from '../../img/cards/KH.png'
+import cardKS from '../../img/cards/KS.png'
+import cardKC from '../../img/cards/KC.png'
+import cardAD from '../../img/cards/AD.png'
+import cardAH from '../../img/cards/AH.png'
+import cardAS from '../../img/cards/AS.png'
+import cardAC from '../../img/cards/AC.png'
+import cardReverse from '../../img/cards/purple_black.png'
+
+const Cards = [
+    {
+        id: '10C',
+        img: card10C
+    },
+    {
+        id: '10D',
+        img: card10D
+    },
+    {
+        id: '10H',
+        img: card10H
+    },
+    {
+        id: '10S',
+        img: card10S
+    },
+    {
+        id: '2C',
+        img: card2C
+    },
+    {
+        id: '2D',
+        img: card2D
+    },
+    {
+        id: '2H',
+        img: card2H
+    },
+    {
+        id: '2S',
+        img: card2S
+    },
+    {
+        id: '3C',
+        img: card3C
+    },
+    {
+        id: '3D',
+        img: card3D
+    },
+    {
+        id: '3H',
+        img: card3H
+    },
+    {
+        id: '3S',
+        img: card3S
+    },
+    {
+        id: '4C',
+        img: card4C
+    },
+    {
+        id: '4D',
+        img: card4D
+    },
+    {
+        id: '4H',
+        img: card4H
+    },
+    {
+        id: '4S',
+        img: card4S
+    },
+    {
+        id: '5C',
+        img: card5C
+    },
+    {
+        id: '5D',
+        img: card5D
+    },
+    {
+        id: '5H',
+        img: card5H
+    },
+    {
+        id: '5S',
+        img: card5S
+    },
+    {
+        id: '6C',
+        img: card6C
+    },
+    {
+        id: '6D',
+        img: card6D
+    },
+    {
+        id: '6H',
+        img: card6H
+    },
+    {
+        id: '6S',
+        img: card6S
+    },
+    {
+        id: '7C',
+        img: card7C
+    },
+    {
+        id: '7D',
+        img: card7D
+    },
+    {
+        id: '7H',
+        img: card7H
+    },
+    {
+        id: '7S',
+        img: card7S
+    },
+    {
+        id: '8C',
+        img: card8C
+    },
+    {
+        id: '8D',
+        img: card8D
+    },
+    {
+        id: '8H',
+        img: card8H
+    },
+    {
+        id: '8S',
+        img: card8S
+    },
+    {
+        id: '9C',
+        img: card9C
+    },
+    {
+        id: '9D',
+        img: card9D
+    },
+    {
+        id: '9H',
+        img: card9H
+    },
+    {
+        id: '9S',
+        img: card9S
+    },
+    {
+        id: 'JC',
+        img: cardJC
+    },
+    {
+        id: 'JD',
+        img: cardJD
+    },
+    {
+        id: 'JH',
+        img: cardJH
+    },
+    {
+        id: 'JS',
+        img: cardJS
+    },
+    {
+        id: 'QC',
+        img: cardQC
+    },
+    {
+        id: 'QD',
+        img: cardQD
+    },
+    {
+        id: 'QH',
+        img: cardQH
+    },
+    {
+        id: 'QS',
+        img: cardQS
+    },
+    {
+        id: 'KC',
+        img: cardKC
+    },
+    {
+        id: 'KD',
+        img: cardKD
+    },
+    {
+        id: 'KH',
+        img: cardKH
+    },
+    {
+        id: 'KS',
+        img: cardKS
+    },
+    {
+        id: 'AC',
+        img: cardAC
+    },
+    {
+        id: 'AD',
+        img: cardAD
+    },
+    {
+        id: 'AH',
+        img: cardAH
+    },
+    {
+        id: 'AS',
+        img: cardAS
+    },
+    {
+        id: 'revers',
+        img: cardReverse
+    }
+]
+
+export default Cards;
---
+++ CardTactics.Web/ClientApp/src/components/Table/Table.js
@@ -0,0 +1,87 @@
+﻿import './Table.css'
+import React from 'react'
+import { styled } from '@mui/material/styles';
+import { Button, Grid, Paper } from '@mui/material';
+import Cards from './Cards'
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    backgroundColor: 'transparent',
+    boxShadow: 'none'
+}));
+
+export const Table = () => {
+    var card = Cards.find(c => c.id = "10C")
+    return (
+        <div id="TableContainer">
+            <div className="CardsContainer">
+                <Grid container spacing={6}>
+                    <Grid item xs={4} md={3}></Grid>
+                    <Grid item xs={6} md={6} id="GridCroupierCards">
+                        <Grid md={2}>
+                            <Item>
+                                <img src={card.img} width='100px' />
+                            </Item>
+                        </Grid>
+                        <Grid md={2}>
+                            <Item>
+                                <img src={card.img} width='100px' />
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={4} md={3}></Grid>
+                    <Grid item xs={4} md={3}></Grid>
+                    <Grid item xs={6} md={6} id="GridCroupierCards">
+                        <Grid md={2}>
+                            <Item>
+                                <img src={card.img} width='100px' />
+                            </Item>
+                        </Grid>
+                        <Grid md={2}>
+                            <Item>
+                                <img src={card.img} width='100px' />
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={4} md={3}></Grid>
+                </Grid>
+            </div>
+            <div className="ButtonContainer">
+                <Grid container spacing={2}>
+                    <Grid item xs={4} md={3}></Grid>
+                    <Grid xs={4} md={6} id="GridButtons">
+                        <Grid md={2}>
+                            <Item>
+                                <Button variant="outlined" color="primary">HIT</Button>
+                            </Item>
+                        </Grid>
+                        <Grid md={2}>
+                            <Item>
+                                <Button variant="outlined" color="secondary">STAND</Button>
+                            </Item>
+                        </Grid>
+                        <Grid md={2}>
+                            <Item>
+                                <Button variant="outlined" color="info">DOUBLE DOWN</Button>
+                            </Item>
+                        </Grid>
+                        <Grid md={2}>
+                            <Item>
+                                <Button variant="outlined" color="error">SPLIT</Button>
+                            </Item>
+                        </Grid>
+                        <Grid md={2}>
+                            <Item>
+                                <Button variant="outlined" color="warning">INSURANCE</Button>
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={4} md={3}></Grid>
+                </Grid>
+            </div>
+        </div>
+    )
+}
